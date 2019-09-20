3e7963d06d6312a2ae277685c946ed85cbfc5e09
juzichas <juzichas@gmail.com>
2019-09-20 15:58:53 -0400

    pushing projects 1-3 up

---
+++ funnewgame/Assets/script.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class movemnt : MonoBehaviour
+{
+    public float speed;
+    public float timer;
+    public GameObject circle; //click and drag circle onto my movment, into CIRCLE space.
+    float posx;
+    float posy;
+    public float freq;
+    public float amp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position);
+        transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+        speed = 5;
+        timer = 0;
+        freq = 1;
+        amp = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(New Vector2(1,0)*
+        //Time.time is constantly going up from start of game. Time.deltatime, is time between frame- usually smaller number
+
+        if (timer >= 1)
+        {
+            transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+        posx = Mathf.Sin(Time.time * freq) * amp;
+        posy = Mathf.Cos(Time.time * freq) * amp;
+        Debug.Log(posx);
+
+        circle.transform.position = new Vector2(transform.position.x + posy, posy + transform.position.y);
+
+    }
+}
---
+++ notebirdy/Assets/WordRecog.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordRecog : MonoBehaviour
+{
+    List<string> letters = new List<string>();
+    string myword
+    string[] mywords = new string ["BEANS","HICKEN", "TOMATO"];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myword = mywords(0,mywords.Length -1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode != KeyCode.None)
+        {
+            Debug.Log("current event detected" + Event.current);
+            letters.Add(Event.current.keyCode.ToString());
+
+            for (int i = 0; i <= letters.Count; i++)
+            {
+
+                if (letters[i] == myword.Substring(1, 1))
+                {
+                    Debug.Log("yay" + letters[1] + "==" + myword.Substring(i, 1));
+
+                    if (letters.Count == myword.Length)
+                    {
+                        Debug.Log("you typed" + myword + "Yay u are cool");
+                        TextAlignment.color = green;
+                    }
+                else
+                {
+                    Debug.Log("boo" + letters[1] + "!=" + myword.Substring(i, 1));
+                    letters.Clear();
+
+                        if (letters.Count != myword.Length)
+                        
+                            Debug.Log("you didn't" + myword + "BOO!!!");
+                        TextAlign, text.text = myword word
+                        }
+                }
+                //foreach (string letter in letters) { Debug.Log(letter); } // printing out 
+            }
+        }
+    }
+}
+
